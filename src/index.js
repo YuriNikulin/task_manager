40261fd83700b3669e9ae22e078012ae4f46462f
Yuri Nikulin <kowkogab@gmail.com>
2018-05-03 18:26:22 +0400

    font has been added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import './design/css/main.css';
 import App from './App';
 import store from './app/redux';
+import './fonts.css';
 
 ReactDOM.render(
   <Provider store={store}>
