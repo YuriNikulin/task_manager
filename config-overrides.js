49f98d4a13265b51bb5d977ec6a1ecc4e73e7044
Yuri Nikulin <kowkogab@gmail.com>
2018-05-15 15:09:01 +0400

    prim color has been changed

--- config-overrides.js
+++ config-overrides.js
@@ -5,7 +5,7 @@ module.exports = function override(config, env) {
     config = injectBabelPlugin(['import', {libraryName: 'antd', libraryDirectory: 'es', style: true}], config);
     config = rewireLess.withLoaderOptions({
         modifyVars: {
-            "@primary-color": "#06AEBC",
+            "@primary-color": "#016ABB",
         }
     })(config, env);
     return config;
