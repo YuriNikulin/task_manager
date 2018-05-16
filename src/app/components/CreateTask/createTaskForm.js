6476a814749a086ea7081a4edb46598e5847818d
Yuri Nikulin <kowkogab@gmail.com>
2018-05-16 17:58:03 +0400

    create task has been partially rewritten

--- src/app/components/CreateTask/createTaskForm.js
+++ src/app/components/CreateTask/createTaskForm.js
@@ -8,9 +8,15 @@ class createTaskForm extends React.Component {
         super(props);
     }
 
+    isNum = (rule, value, callback) => {
+        if (!Number(value)) callback('Must be a number!');
+        callback();
+    }
+
     render() {
         const { getFieldDecorator, getFieldsError, getFieldError, isFieldTouched } = this.props.form;
         const nameError = isFieldTouched('name') && getFieldError('name');
+        const estimatedError = isFieldTouched('estimated') && getFieldError('estimated');
         return (
             <Form 
                 layout="vertical"
@@ -31,6 +37,27 @@ class createTaskForm extends React.Component {
                         ]
                     })(
                         <Input placeholder="Task name" />
+                    )}
+                </FormItem>
+                <FormItem 
+                    validateStatus={estimatedError ? 'error' : ''}
+                    help={estimatedError || ''}
+                    label="Estimated"
+                    labelCol={{sm: {span: 10}}}
+                    wrapperCol={{sm: {span: 14}}}>
+                    
+                    {getFieldDecorator('estimated', {
+                        rules: [
+                        {
+                            required: true,
+                            message: 'Please input estimated time!'
+                        },
+                        {
+                            validator: this.isNum
+                        },
+                        ]
+                    })(
+                        <Input placeholder="Estimated time" />
                     )}
                 </FormItem>
                     
