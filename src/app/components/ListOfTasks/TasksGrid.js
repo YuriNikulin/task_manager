b6aa99a97c56880d999fa365e9e39359f81d47b4
Yuri Nikulin <kowkogab@gmail.com>
2018-05-21 17:37:11 +0400

    grid has been rewritten with ant

--- src/app/components/ListOfTasks/TasksGrid.js
+++ src/app/components/ListOfTasks/TasksGrid.js
@@ -1,34 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { Card, Col, Row } from 'antd';
 
 const TasksGrid = (props) => {
     return (
         <div className="tm-grid">
-            {props.tasksList.map((item) => {
-                return (
-                    <Link key={item.taskId} to={"/task" + item.taskId} className="tm-grid-item">
-                        <div className="tm-grid-item-container tm-grid-item-name">
-                            <span className="tm-grid-item__name">
-                                <span className="tm-tasks__link">
-                                    {item.taskName}
-                                </span>    
-                            </span>
-                        </div>
-                        <div className="tm-grid-item-container tm-grid-item-status">
-                            Status:
-                            <span className="tm-grid-item__status">
-                                {item.taskStatus}
-                            </span>
-                        </div>
-                        <div className="tm-grid-item-container tm-grid-item-priority">
-                            Priority:
-                            <span className="tm-grid-item__priority">
-                                {item.taskPriority}
-                            </span>
-                        </div>
-                    </Link>    
-                )
-            })}
+            <Row gutter={10}>
+                {props.tasksList.map((item) => {
+                    return (
+                        <Col key={item.taskId} xl={6} lg={8} md={12} sm={24}>
+                            <Card title={item.taskName} className="tm-grid-item">
+                                <div className="tm-grid-item-container tm-grid-item-status">
+                                    Status:
+                                    <span className="tm-grid-item__status">
+                                        {item.taskStatus}
+                                    </span>
+                                </div>
+                                <div className="tm-grid-item-container tm-grid-item-priority">
+                                    Priority:
+                                    <span className="tm-grid-item__priority">
+                                        {item.taskPriority}
+                                    </span>
+                                </div>
+                                <Link className="tm-grid-item__overlay" to={`/task${item.taskId}`} />
+                            </Card>
+                        </Col>
+                    )
+                })}
+            </Row>
         </div>
     )    
 }
